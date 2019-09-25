19e75f1f5846b26507ffe051c1db69aa3cb238ca
Ilton <xlsonplay@gmail.com>
2019-09-25 15:07:54 -0300

    adiciona funções adicionais para os contadores e frequencias

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,21 @@ function contador2() {
     document.getElementById("valor").innerHTML = "Valor: " + pontuacao;
 }
 
+function contador3() {
+    pontuacao += 20;
+    document.getElementById("valor").innerHTML = "Valor: " + pontuacao;
+}
+
 function frequencia(){
-    if(pontuacao >= 50){
+    if(pontuacao > 50){
         var texto = "Você passou de 50, ual em";
         document.getElementById("motivador").innerHTML = texto;
     }
 }
 
+function frequencia2() {
+    if (pontuacao >= 100) {
+        var texto = "Sucesso, ja chegou em 100? caramba em";
+        document.getElementById("motivador").innerHTML = texto;
+    }
+}
